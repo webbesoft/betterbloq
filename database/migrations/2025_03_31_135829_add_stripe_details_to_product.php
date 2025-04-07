3b39db5a99d3b2a9e55aa7a8ac387cc9a8413d3d
Tawanda <ejmunongo@gmail.com>
2025-04-07 15:37:25 +0200

    BBx - fix permissions

--- database/migrations/2025_03_31_135829_add_stripe_details_to_product.php
+++ database/migrations/2025_03_31_135829_add_stripe_details_to_product.php
@@ -14,8 +14,8 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             //
-            $table->string('stripe_product_id')->default(DB::raw('(UUID())'));
-            $table->string('stripe_price_id')->default(DB::raw('(UUID())'));
+            $table->string('stripe_product_id')->default("product_");
+            $table->string('stripe_price_id')->default("price_");
         });
     }
 
