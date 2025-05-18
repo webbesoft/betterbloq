295162be892edf3e6be01a3c9321ed77a8439ad1
Tawanda <ejmunongo@gmail.com>
2025-05-18 10:29:15 +0200

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { resolve } from 'node:path';
+import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { watchAndRun } from 'vite-plugin-watch-and-run';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx', 'resources/css/prezet.css'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
         react(),
         tailwindcss(),
+        watchAndRun([
+            {
+                name: 'prezet:index',
+                watch: path.resolve('prezet/**/*.(md|jpg|png|webp)'),
+                run: 'php artisan prezet:index',
+                delay: 1000,
+                // watchKind: ['add', 'change', 'unlink'], // (default)
+            },
+        ]),
     ],
     esbuild: {
         jsx: 'automatic',
